d2675299885637ee3193b184efbfb8afb94fb0cc
strahinjaEmotion <S.Velickovic@emotion-home.eu>
2021-02-03 20:03:36 +0100

    Get and Clear contacts

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -7,13 +7,21 @@ import {
   FILTER_CONTACTS,
   CLEAR_FILTER,
   CONTACT_ERROR,
+  GET_CONTACTS,
+  CLEAR_CONTACTS,
 } from '../types';
 
 export default (state, action) => {
   switch (action.type) {
+    case GET_CONTACTS:
+      return { ...state, contacts: action.payload, loading: false };
     case ADD_CONTACT:
       //ne mozemo samo contacts.push jer je state nepromenljiv moramo da zalepimo sve prethodno pa da dodamo
-      return { ...state, contacts: [...state.contacts, action.payload] };
+      return {
+        ...state,
+        contacts: [...state.contacts, action.payload],
+        loading: false,
+      };
 
     case UPDATE_CONTACT:
       return {
@@ -22,6 +30,7 @@ export default (state, action) => {
         contacts: state.contacts.map((contact) =>
           contact.id === action.payload.id ? action.payload : contact
         ),
+        loading: false,
       };
 
     case DELETE_CONTACT:
@@ -30,8 +39,16 @@ export default (state, action) => {
         contacts: state.contacts.filter(
           (contact) => contact.id != action.payload
         ),
+        loading: false,
       };
-
+    case CLEAR_CONTACTS:
+      return {
+        ...state,
+        contacts: null,
+        filtered: null,
+        error: null,
+        current: null,
+      };
     case SET_CURRENT:
       return { ...state, current: action.payload };
 
